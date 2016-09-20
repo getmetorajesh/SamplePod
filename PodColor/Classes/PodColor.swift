5397b29554c53425026efb5edb2064b88ac86b0f
rj <getmetorajesh@gmail.com>
2016-09-20 14:51:18 +1000

    new pro

---
+++ PodColor/Classes/PodColor.swift
@@ -0,0 +1,18 @@
+import Foundation
+import UIKit
+
+@objc class PodColor: NSObject {
+    public func RGBUIColor(red red: Int, green: Int, blue: Int) -> UIColor {
+        return UIColor.blueColor()
+    }
+    
+    public func createColor(red: Int, green: Int, blue: Int) -> UIColor {
+        return UIColor(
+            red: CGFloat(red/255),
+            green: CGFloat(green/255),
+            blue: CGFloat(blue/155),
+            alpha: 1
+        )
+    }
+    
+}
